243323553669554baadff9d2f985c11f2989bd50
Mikhail <wowivan2011@yandex.ru>
2021-06-29 17:48:03 +0300

    Added users counting

--- Sources/App/Controllers/WebSocketController.swift
+++ Sources/App/Controllers/WebSocketController.swift
@@ -9,22 +9,26 @@ import Vapor
 
 class WebSocketController: RouteCollection {
     
-    public var sockets: [WebSocketConnectionModel] = []
+    private var sockets: [WebSocketConnectionModel] = []
     
     func boot(routes: RoutesBuilder) throws {
         routes.webSocket("chat", ":userId") { req, wSocket in
             if let userId = req.parameters.get("userId"),
                let uuid = UUID(uuidString: userId) {
-
+                
                 let idConnection = UUID()
                 self.sockets.append(.init(id: idConnection, socket: wSocket, userId: uuid))
                 
-                req.logger.info("websocket: User \(uuid.uuidString) connected. Connection ID: \(idConnection)")
+                self.updateUserCount()
+                
                 
                 wSocket.onClose.map {
                     req.logger.info("websocket: connection \(idConnection) closed")
                     self.sockets.removeAll { $0.id == idConnection }
-                }.whenComplete {_ in}
+                    
+                }.whenComplete {_ in
+                    self.updateUserCount()
+                }
                 
                 wSocket.onText { _, message in
                     let jsonData = Data(message.utf8)
@@ -41,12 +45,20 @@ class WebSocketController: RouteCollection {
                         print(error.localizedDescription)
                     }
                 }
-
+                
+                req.logger.info("websocket: User \(uuid.uuidString) connected. Connection ID: \(idConnection)")
+                
             } else {
                 req.logger.warning("websocket: Invalid user id")
                 wSocket.send("Invalid user id")
                 wSocket.close().whenComplete {_ in}
             }
+        }
+    }
+    
+    func updateUserCount() {
+        sockets.forEach { model in
+            model.socket.send("\(sockets.count)")
         }
     }
 }
